94cb2a69e4cd04b3fc56f15a977c54004295f5f1
gtsimis <gt2186@gmail.com>
2021-09-28 17:37:34 -0400

    Added myMap

--- First_JS_proj/phase_2_enumerable.js
+++ First_JS_proj/phase_2_enumerable.js
@@ -10,9 +10,20 @@ Array.prototype.myEach = function(callback) {
 //     names.forEach(name => console.log(name));
 // })
 arr1 = ["Mary", "Brian", "Leo"]
+arr2 = ["Billy", "Joe", "Tiff"]
 
 // function namesFunc(name) {
 //     ;
 // }
 
-arr1.myEach((name) => { console.log(name.toUpperCase());});
+// arr1.myEach((name) => { console.log(name.toUpperCase());});
+
+Array.prototype.myMap = function (callback) {
+    const newArr = [];
+    
+    this.myEach(el => newArr.push(callback(el)));
+
+    return newArr;
+} 
+
+console.log(arr2.myMap(name => name.toUpperCase()))
